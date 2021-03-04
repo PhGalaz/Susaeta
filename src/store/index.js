4ef19666ce46f52b6d4c3107c724002cae687105
Kiltro <felipe@galaz.de>
2021-03-04 15:55:01 -0300

    first

--- src/store/index.js
+++ src/store/index.js
@@ -18,7 +18,7 @@ export default new Vuex.Store({
           name: "Casa Riscos",
           location: "Puerto Varas",
           year: "2012",
-          pictures: ["casariscos3.jpg","casariscos6.jpg","casariscos7.jpg"],
+          pictures: ["prueba1.jpeg","prueba5.jpg","prueba6.jpg","prueba2.jpeg"],
           description1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
@@ -26,7 +26,7 @@ export default new Vuex.Store({
           name: "Casa Riscos",
           location: "Puerto Varas",
           year: "2012",
-          pictures: ["b01.jpg","b02.jpg","b03.jpg","b04.jpg"],
+          pictures: ["prueba1.jpeg","prueba5.jpg","prueba6.jpg","prueba2.jpeg"],
           description1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
@@ -34,7 +34,7 @@ export default new Vuex.Store({
           name: "Casa Riscos",
           location: "Puerto Varas",
           year: "2012",
-          pictures: ["casamarina.jpg","IMG_9315.JPG","IMG_9320.JPG","IMG_9326.JPG","IMG_9338.JPG","IMG_9345.JPG"],
+          pictures: ["prueba1.jpeg","prueba5.jpg","prueba6.jpg","prueba2.jpeg"],
           description1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
@@ -42,7 +42,7 @@ export default new Vuex.Store({
           name: "Casa Riscos",
           location: "Puerto Varas",
           year: "2012",
-          pictures: ["c04.jpg","c01.jpg","c02.jpg","c03.jpg","c05.jpg","c06.jpg"],
+          pictures: ["prueba1.jpeg","prueba5.jpg","prueba6.jpg","prueba2.jpeg"],
           description1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
@@ -50,7 +50,7 @@ export default new Vuex.Store({
           name: "Casa Riscos",
           location: "Puerto Varas",
           year: "2012",
-          pictures: ["d05.jpg","d01.jpg","d02.jpg","d03.jpg","d04.jpg","d07.jpg","d08.jpg","d09.jpg","d10.jpg"],
+          pictures: ["prueba1.jpeg","prueba5.jpg","prueba6.jpg","prueba2.jpeg"],
           description1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       },
       {
@@ -58,7 +58,7 @@ export default new Vuex.Store({
           name: "Casa Riscos",
           location: "Puerto Varas",
           year: "2012",
-          pictures: ["e04.jpg","e05.jpg","e01.jpg","e02.jpg","e03.jpg"],
+          pictures: ["prueba1.jpeg","prueba5.jpg","prueba6.jpg","prueba2.jpeg"],
           description1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       }
     ]
