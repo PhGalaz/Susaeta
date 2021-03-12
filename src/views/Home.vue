6919cfebb75c2fa972e91be136f29e923c022f18
Kiltro <felipe@galaz.de>
2021-03-11 21:54:21 -0300

    first

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -266,7 +266,7 @@
           cols="6"
         >
           <p
-            class="ma-5 ml-10 mr-16 pr-16 parr"
+            class="ma-5 ml-10 mr-16 pr-16 parr d-none d-sm-flex"
             style="font-size:35px"
           >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </v-col>
