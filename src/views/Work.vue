eec3c19165de141ca180d4f54c51388d4cbb6956
Kiltro <felipe@galaz.de>
2021-03-16 07:34:19 -0300

    work navigation in progress

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -108,7 +108,7 @@
     },
     methods: {
       onSwipeUp: async function () {
-        window.scrollTo(0, 1);
+        window.scrollTo(0, 100);
         if (this.blokeo == false){
           this.blokeo = true
           this.model = this.model + 1 % this.$store.state.projects.length
