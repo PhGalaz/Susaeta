cd379f0e0662d9444a7315eedf14448f66b96e08
Kiltro <felipe@galaz.de>
2021-03-17 23:49:08 -0300

    work scroll

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -2,7 +2,7 @@
 
   <v-row
     class="ma-0 pa-0"
-    style="position:absolute;background-color:#707070;height:200vh"
+    style="position:absolute;background-color:#707070;height:100vh"
 
 
   >
@@ -101,7 +101,7 @@
 
     },
     mounted () {
-
+      window.requestFullscreen() 
     },
     beforeDestroy () {
       window.removeEventListener('scroll', this.onScroll)
